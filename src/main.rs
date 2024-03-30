56363b47f6d4ff5761770f8d1ca42bb1d6de0bce
Zhao Ziloong <ziloon.z@icloud.com>
2024-03-31 00:40:55 +0800

    定义枚举

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    route(four);
+    route(six);
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+}
+
+fn route(ip_kind: IpAddrKind) {
+    println!("{:#?}", ip_kind);
 }
