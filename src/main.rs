bfd577e65fb97e1a5c0cf66ff5c8cc8c03898746
Zhao Ziloong <ziloon.z@icloud.com>
2024-03-31 00:57:40 +0800

    定义挂接不同数据的枚举。

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,25 @@ enum IpAddrKind {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     let home = IpAddrKind::V4(127,0,0,1);
     let loopback = IpAddrKind::V6(String::from("::1"));
     route(home);
     route(loopback);
+
+    let _q = Message::Quit;
+    let _m = Message::Move { x: 12, y: 24 };
+    let _w = Message::Write(String::from("Hello"));
+    let _c = Message::ChangeColor(0, 255, 255);
 }
 
 fn route(ip_kind: IpAddrKind) {
-    println!("{:#?}", ip_kind);
+    println!("route to {:#?}", ip_kind);
 }
