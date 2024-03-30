5ed6f8793551d5aee94cd8ee393408307ce9787c
Zhao Ziloong <ziloon.z@icloud.com>
2024-03-31 00:45:03 +0800

    作为struct的成员

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ enum IpAddrKind {
     V6,
 }
 
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
@@ -11,6 +16,16 @@ fn main() {
     route(six);
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
 
 fn route(ip_kind: IpAddrKind) {
