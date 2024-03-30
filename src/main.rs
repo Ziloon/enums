ab49f89f80a1158c6ed78567ffd0c034967d464b
Zhao Ziloong <ziloon.z@icloud.com>
2024-03-31 00:50:51 +0800

    将数据附加到枚举的变体中

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
 #[derive(Debug)]
 enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-    route(four);
-    route(six);
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
-
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddrKind::V4(127,0,0,1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+    route(home);
+    route(loopback);
 }
 
 fn route(ip_kind: IpAddrKind) {
